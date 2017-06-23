645274ee6d786292d16d3f87e51be362c7edcf70
Alan Hong <xiupan@gmail.com>
2017-06-22 19:04:51 -0500

    Framework for creating and selecting random words for varying gamemodes.

---
+++ hangman.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const expressSession = require('express-session');
+const mustacheExpress = require('mustache-express');
+const parseUrl = require('parseurl');
+const bodyParser = require('body-parser');
+
+var fs = require('fs');
+
+var Busboy = require('busboy');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(express.static('/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000, function () {
+  console.log('Successfully started Hangman application!');
+});
+
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const easyWords = [];
+const normalWords = [];
+const hardWords = [];
+
+// for loop that creates different arrays of varying difficulty words
+for (let i = 0; i < words.length; ++i) {
+  if(words[i].length >= 4 && words[i].length <= 6){
+    easyWords.push(words[i]);
+  } else if(words[i].length >= 6 && words[i].length <= 8){
+    normalWords.push(words[i]);
+  } else if(words[i].length >= 8){
+    hardWords.push(words[i]);
+  }
+}
+
+// function to grab a random integer between a specified min and max
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// uses the random integer function to select a random word from the proper array of words
+var randomEasyWord = easyWords[getRandomIntInclusive(0, easyWords.length)];
+var randomNormalWord = normalWords[getRandomIntInclusive(0, normalWords.length)];
+var randomHardWord = hardWords[getRandomIntInclusive(0, hardWords.length)];
+
+// console.log(randomEasyWord, randomNormalWord, randomHardWord);
+
+// console.log(easyWords.length, normalWords.length, hardWords.length);
